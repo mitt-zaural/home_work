4349810510b7325b75fa040bd6d8ee4a5000185a
AlSy <mitt@zaural.ru>
2022-10-18 05:27:22 +0500

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -4,40 +4,44 @@ import (
 	"errors"
 	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-	var countX int
+	var countNext int
 	var err error
 	strlen := len(s)
 
 	result := ""
-	for i := 0; i < strlen; i++ {
-		c := s[i : i+1] // выделяем символ из строки
-		if c != "\\" {
-			_, err = strconv.Atoi(c)
+	for i := 0; i < strlen; {
+		c, rsize := utf8.DecodeRuneInString(s[i:]) // выделяем руну из строки
+		if string(c) != "\\" {
+			_, err = strconv.Atoi(string(c))
 		} else {
-			i++
+			i += rsize
 			err = ErrInvalidString
 		}
 		if err == nil { // удачно сконвертировали - значит первый символ цифра, что является ошибкой
 			return "", ErrInvalidString // возвращаем пустую строку и строку описания ошибки
 		}
-		if i < strlen-1 { // индекс ещё в диапазоне строки?
-			c1 := s[i+1 : i+2]             // читаем следующий символ
-			countX, err = strconv.Atoi(c1) // следующий символ - это число?
-			if err == nil && countX != 0 { // да, это ненулевое число
-				result += strings.Repeat(c, countX-1)
-				i++ // увеличим индекс, перескочим число
+		if i < strlen-1 { // индекс ещё в диапазоне длины строки?
+			c1, rsize2 := utf8.DecodeRuneInString(s[i+rsize:]) // читаем следующую руну
+			countNext, err = strconv.Atoi(string(c1))          // следующая руна - это число?
+			if err == nil && countNext != 0 {                  // да, это ненулевое число
+				result += strings.Repeat(string(c), countNext-1)
+				i += rsize2 // увеличим индекс, перескочим число
 			}
-			if err == nil && countX == 0 { // это нулевое число
-				i++ // увеличим индекс, перескочим число
-				c = ""
+			if err == nil && countNext == 0 { // это нулевое число
+				i += rsize2 // увеличим индекс, перескочим число
+				c = 0
 			}
 		}
-		result += c
+		if c != 0 {
+			result += string(c)
+		}
+		i += rsize // увеличиваем индекс с учетом размера обработанной руны
 	}
 	return result, nil
 }
